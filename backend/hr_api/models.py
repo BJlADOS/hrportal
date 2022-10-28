c9201deb647cb12954841310bb9f4d0f07c2adb0
angst-storm <serezha.kiprin@gmail.com>
2022-10-28 17:28:23 +0500

    add department model

--- backend/hr_api/models.py
+++ backend/hr_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from jwt_auth.models import User
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    manager = models.OneToOneField(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
