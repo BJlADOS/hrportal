fc4084bce6f8629933998a4bde30fcea4633b237
angst-storm <serezha.kiprin@gmail.com>
2022-12-16 20:03:56 +0500

    dockerize django (and postgres)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': '5432',
     }
 }
 
